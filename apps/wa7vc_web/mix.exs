041809e34e18d0f081780742ded20851c758027f
Josh Proehl <josh@daedalusdreams.com>
2018-08-24 10:12:53 -0700

    Version bump apps to 0.0.3

--- apps/wa7vc_web/mix.exs
+++ apps/wa7vc_web/mix.exs
@@ -4,7 +4,7 @@ defmodule Wa7vcWeb.Mixfile do
   def project do
     [
       app: :wa7vc_web,
-      version: append_revision("0.0.2"),
+      version: append_revision("0.0.3"),
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
