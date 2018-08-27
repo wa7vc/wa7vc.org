5c628e0b4d46f913ba103f7ec0e6050395b91381
Josh Proehl <josh@daedalusdreams.com>
2018-08-27 12:36:57 -0700

    Add logging to webhook handler

--- apps/marvin/lib/marvin/github_webhook_plug.ex
+++ apps/marvin/lib/marvin/github_webhook_plug.ex
@@ -2,6 +2,7 @@
 
 defmodule Marvin.GithubWebhookPlug do
   import Plug.Conn
+  require Logger
   
   def init(options) do
     options
@@ -29,6 +30,7 @@ defmodule Marvin.GithubWebhookPlug do
       x -> x
     end
     hmac = :crypto.hmac(:sha, key, body)
+    Logger.info("Handling Github Webhook. Using key: #{key}, expecting #{Base.encode16(signature, case: :lower)}, calculated #{Base.encode16(hmac, case: :lower)}")
     case hmac do
       ^signature ->
         hook = Poison.decode!(body)
@@ -41,7 +43,7 @@ defmodule Marvin.GithubWebhookPlug do
         IO.inspect key, label: "Using Key: "
         conn
         |> put_resp_content_type("application/json")
-        |> send_resp(401, "{ \"code\":401, \"msg\":\"You're not github!\", \"calculated\":\"#{hmac}\", \"given\":\"#{signature}\" }")
+        |> send_resp(401, "{ \"code\":401, \"msg\":\"You're not github!\", \"calculated\":\"#{Base.encode16(hmac, case: :lower)}\", \"given\":\"#{Base.encode16(signature, case: :lower)}\" }")
         |> halt
     end
   end
