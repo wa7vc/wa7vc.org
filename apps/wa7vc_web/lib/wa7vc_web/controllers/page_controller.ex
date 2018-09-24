3704828c24a28a4ba8f7f39727a81274e40959b8
Josh Proehl <josh@daedalusdreams.com>
2018-09-24 11:04:07 -0700

    Move SummerGathering URL, add per-year pages

--- apps/wa7vc_web/lib/wa7vc_web/controllers/page_controller.ex
+++ apps/wa7vc_web/lib/wa7vc_web/controllers/page_controller.ex
@@ -5,7 +5,19 @@ defmodule Wa7vcWeb.PageController do
     render conn, "index.html"
   end
 
-  def nwaprssg(conn, _params) do
-    render conn, "nwaprssg.html"
+  # If no year given redirect to current year. This will need to change when we add the next year!
+  def summergathering(conn, params) do
+    case params["year"] do
+      nil -> redirect conn, to: "/summergathering/2019"
+      year ->
+        try do
+          render conn, "summergathering-#{year}.html"
+        rescue
+          Phoenix.Template.UndefinedError ->  conn
+                                                |> put_flash(:error, "No Summer Gathering page for the year #{year} yet, sorry!")
+                                                |> redirect(to: "/summergathering")
+                                                |> halt()
+        end
+    end
   end
 end
--- apps/wa7vc_web/lib/wa7vc_web/router.ex
+++ apps/wa7vc_web/lib/wa7vc_web/router.ex
@@ -17,7 +17,10 @@ defmodule Wa7vcWeb.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    get "/nwaprssg", PageController, :nwaprssg
+
+    get "/summergathering/:year", PageController, :summergathering
+    get "/summergathering", PageController, :summergathering
+    get "/nwaprssg", PageController, :summergathering
   end
 
   # Other scopes may use custom stacks.
