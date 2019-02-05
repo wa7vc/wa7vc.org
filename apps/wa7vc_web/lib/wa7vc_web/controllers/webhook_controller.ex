c01704e64839e6d86edce93fc9d14f27a2369d97
Josh Proehl <josh@daedalusdreams.com>
2019-02-05 10:14:01 -0800

    Remove legacy pre-plug webhook controller

--- apps/wa7vc_web/lib/wa7vc_web/controllers/webhook_controller.ex
+++ apps/wa7vc_web/lib/wa7vc_web/controllers/webhook_controller.ex
@@ -7,18 +7,4 @@ defmodule Wa7vcWeb.WebhookController do
     render conn, "index.html"
   end
 
-  def github_webhook(conn, params) do
-    event_type = get_req_header(conn, "x-github-event") |> List.first()
-    signature = get_req_header(conn, "x-hub-signature") |> List.first()
-
-    body = read_body(conn)
-
-    case Marvin.GithubWebhookHandler.handle_hook(signature, body) do
-      {:ok} ->
-        Wa7vcWeb.Endpoint.broadcast! "website:pingmsg", "message", %{ :text => "Got a #{event_type} event from GitHub!" }
-        render conn, "github_webhook_response.json", %{event_type: event_type}
-      _ -> render conn, "github_webhook_error.json", %{}
-    end
-
-  end
 end
