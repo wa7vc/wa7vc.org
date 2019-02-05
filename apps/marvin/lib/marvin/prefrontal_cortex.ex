44d2d447a95b2d8627bcedbe09cd9c55de049d85
Josh Proehl <josh@daedalusdreams.com>
2019-02-05 11:26:57 -0800

    Handle errors in PrefrontalCortex, add getcounter

--- apps/marvin/lib/marvin/prefrontal_cortex.ex
+++ apps/marvin/lib/marvin/prefrontal_cortex.ex
@@ -8,15 +8,32 @@ defmodule Marvin.PrefrontalCortex do
     GenServer.start_link(__MODULE__, [], name: __MODULE__)
   end
 
+  # Insert a {key,value} tuple into, updated the value of the key already exists.
   def put(key, value) do
     :ets.insert(__MODULE__, {key, value})
   end
 
+  # Get the value for the given key, stripping it out of the {key,value} tuple.
+  # Returns :error if key isn't found.
   def get(key) do
-    [{_key, val}] = :ets.lookup(__MODULE__, key)
-    val
+    case :ets.lookup(__MODULE__, key) do
+      [{^key, val}] -> val
+      [] -> :error
+    end
   end
 
+  # Get a value that's assumed to be a counter.
+  # Because the counter may not have been created yet, we'll default to 0 if the key isn't found.
+  def getcounter(key) do
+    case val = get(key) do
+      :error -> 0
+      _ -> val
+    end
+  end
+
+  # Update a counter (atomically) at the given key. Defaults the value to 0 if the key doesn't already exist.
+  # Defaults to incrementing the value by 1, unless you provide an amount to increment by
+  # Returns the new value of the counter.
   def increment(key, amount \\ 1) do
     :ets.update_counter(__MODULE__, key, amount, {key, 0})
   end
