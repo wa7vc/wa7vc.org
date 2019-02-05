a77a36c9ea4bc9ad82320562717d1c53f9859c54
Josh Proehl <josh@daedalusdreams.com>
2019-02-05 14:43:26 -0800

    Fix MarvinMisc responders command order
    Turns out you have to return the result of the send/reply/msg in order
    to prevent errors.

--- apps/marvin/lib/responders/MarvinMisc.ex
+++ apps/marvin/lib/responders/MarvinMisc.ex
@@ -15,23 +15,23 @@ defmodule Hedwig.Responders.MarvinMisc do
   hedwig: ShapeUp - Bot may or may not become snarky.
   """
   respond ~r/ShapeUp(!)?/i, msg do
-    reply msg, "But I've got this terrible pain in all the diodes down my left side..."
-    Wa7vcWeb.Endpoint.broadcast! "website:pingmsg", "message", %{ :text => "#{msg.user.name} in #{msg.room} just told me to shape up, but all my diodes hurt and it made me sad." }
     Marvin.PrefrontalCortex.increment(:irc_interactions_count)
+    Wa7vcWeb.Endpoint.broadcast! "website:pingmsg", "message", %{ :text => "#{msg.user.name} in #{msg.room} just told me to shape up, but all my diodes hurt and it made me sad." }
+    reply msg, "But I've got this terrible pain in all the diodes down my left side..."
   end
 
   @usage """
   hedwig: uptime - Report how long the server has been running
   """
-  respond ~r/uptime/i, msg do
-    reply msg, "The last time I was awakened was #{Marvin.Application.last_started()}, around #{Marvin.Application.lifespan(:marvinyears) |> Number.Human.number_to_human} years ago from my perspective."
-    Wa7vcWeb.Endpoint.broadcast! "website:pingmsg", "message", %{ :text => "#{msg.user.name} in #{msg.room} just impolitely asked my age." }
+  respond ~r/uptime$/i, msg do
     Marvin.PrefrontalCortex.increment(:irc_interactions_count)
+    Wa7vcWeb.Endpoint.broadcast! "website:pingmsg", "message", %{ :text => "#{msg.user.name} in #{msg.room} just impolitely asked my age." }
+    reply msg, "The last time I was awakened was #{Marvin.Application.last_started()}, around #{Marvin.Application.lifespan(:marvinyears) |> Number.Human.number_to_human} years ago from my perspective."
   end
 
   hear ~r/notice me senpai/i, msg do
-    send msg, "#{msg.user.name} HAS BEEN NOTICED"
-    Wa7vcWeb.Endpoint.broadcast! "website:pingmsg", "message", %{ :text => "In #{msg.room} #{msg.user.name} wanted to be noticed, so I obliged." }
     Marvin.PrefrontalCortex.increment(:irc_interactions_count)
+    Wa7vcWeb.Endpoint.broadcast! "website:pingmsg", "message", %{ :text => "In #{msg.room} #{msg.user.name} wanted to be noticed, so I obliged." }
+    send msg, "#{msg.user.name} HAS BEEN NOTICED"
   end
 end
