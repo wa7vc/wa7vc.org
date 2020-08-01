43e07a147f251c0f18a00369a59ca88d05dedeed
Josh Proehl <josh@daedalusdreams.com>
2020-08-01 12:52:55 -0500

    Add per-page meta descriptions

--- apps/wa7vc_web/lib/wa7vc_web/controllers/page_controller.ex
+++ apps/wa7vc_web/lib/wa7vc_web/controllers/page_controller.ex
@@ -2,7 +2,9 @@ defmodule Wa7vcWeb.PageController do
   use Wa7vcWeb, :controller
 
   plug :sg_year when action in [:summergathering]
+  plug :sg_meta_desc when action in [:summergathering]
   plug :dmr_year when action in [:dmrgathering]
+  plug :dmr_meta_desc when action in [:dmrgathering]
 
   defp dmr_year(conn, _opts) do
     valid_years = ["2019", "2020"]
@@ -21,6 +23,15 @@ defmodule Wa7vcWeb.PageController do
         |> put_flash(:error, "No DMR Gathering page for #{requested_year}, sorry!")
         |> assign(:load_year, latest_year)
     end
+  end
+
+  defp dmr_meta_desc(conn, _opts) do
+    desc = case conn.assigns.load_year do
+      "2020" -> "Cancelled due to Covid-19. Check back soon for 2021 dates and agenda."
+      _ -> "The Pacific Northwest Digital Mobile Radio (WA7DMR) yearly gathering, the best event to find out the latest greatest information about DMR!"
+    end
+    conn
+    |> assign(:meta_attrs, [ %{name: "description", content: desc} ])
   end
 
   defp sg_year(conn, _opts) do
@@ -42,9 +53,18 @@ defmodule Wa7vcWeb.PageController do
     end
   end
 
+  defp sg_meta_desc(conn, _opts) do
+    desc = case conn.assigns.load_year do
+      "2020" -> "2020 Summer Gathering cancelled due to Covid-19. Check back soon for 2021 Dates and Agenda!"
+      _ -> "Our signature event, the Summer Gathering brings hams together for presentations, troubleshooting sessions, food, friends, and antennas!"
+    end
+    conn
+    |> assign(:meta_attrs, [ %{name: "description", content: desc} ])
+  end
 
   def index(conn, _params) do
-    render conn, "index.html"
+    meta_attrs_list = [ %{name: "description", content: "Ham Radio in the Snoqualamie Middle Fork Valley. Find out more about us and our schedule of events."} ]
+    render conn, "index.html", meta_attrs: meta_attrs_list
   end
 
   def dmrgathering(conn, _params) do
@@ -72,11 +92,16 @@ defmodule Wa7vcWeb.PageController do
     #last_start_ts = NaiveDateTime.utc_now() |> NaiveDateTime.add(last_started_ago*-1, :millisecond)
     #last_start_lifespan = NaiveDateTime.diff(NaiveDateTime.utc_now(), last_start_ts)
 
+    meta_attrs_list = [ %{name: "robots", content: "noindex"},
+                        %{name: "description", content: "Statistics and interesting facts, with a healthy dose of depression."}
+                      ]
+
 
     render conn, "marvin.html",
       wa7vc_web_version: vsn_wa7vc_web,
       wa7vc_version: vsn_wa7vc,
-      marvin_version: vsn_marvin
+      marvin_version: vsn_marvin,
+      meta_attrs: meta_attrs_list
   end
 
 
