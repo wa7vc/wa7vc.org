017725a92436244c9be7780b237915fb2760df65
Josh Proehl <josh@daedalusdreams.com>
2020-10-13 11:02:36 -0500

    Update releaser for webpack, version bump

--- apps/wa7vc/mix.exs
+++ apps/wa7vc/mix.exs
@@ -4,7 +4,7 @@ defmodule Wa7vcWeb.Mixfile do
   def project do
     [
       app: :wa7vc,
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
