2484196bae1ee6b0c3ac57f41234c33931411537
Josh Proehl <josh@daedalusdreams.com>
2020-10-14 09:26:38 -0500

    add github webhook plug test

--- apps/wa7vc/config/test.exs
+++ apps/wa7vc/config/test.exs
@@ -7,4 +7,5 @@ config :logger, level: :warn
 # you can enable the server option below.
 config :wa7vc, Wa7vcWeb.Endpoint,
   http: [port: 4001],
-  server: false
+  server: false,
+  github_webhook_secret: "LK44/Ayqlqr8oBB0mzKyRX5aQBaxU1WE4fvae5VlIHrYq8mYnqLcAOPAX8jtU4A5" 
