25cb95860138060a792add3c6b76a660ea926294
Josh Proehl <josh@daedalusdreams.com>
2020-10-14 12:45:00 -0500

    Fix page controller test stub to not fail

--- apps/wa7vc/test/wa7vc_web/controllers/page_controller_test.exs
+++ apps/wa7vc/test/wa7vc_web/controllers/page_controller_test.exs
@@ -1,8 +1,9 @@
 defmodule Wa7vcWeb.PageControllerTest do
   use Wa7vcWeb.ConnCase
 
+  # Sanity check front page rendering
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "WA7VC, the Valley Camp Amateur Radio Group"
   end
 end
