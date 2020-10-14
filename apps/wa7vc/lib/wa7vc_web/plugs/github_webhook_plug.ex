c78a71408eb7e0b685cd82364c4a3d833345a5fd
Josh Proehl <josh@daedalusdreams.com>
2020-10-14 13:23:19 -0500

    Refactor github webhook receiver.

--- apps/wa7vc/lib/wa7vc_web/plugs/github_webhook_plug.ex
+++ apps/wa7vc/lib/wa7vc_web/plugs/github_webhook_plug.ex
@@ -1,4 +1,5 @@
-# Used modified from https://github.com/hamiltop/ashes/blob/master/lib/ashes/github_webhook_plug.ex
+# Used concept modified from
+# https://github.com/hamiltop/ashes/blob/master/lib/ashes/github_webhook_plug.ex
 defmodule Wa7vcWeb.Plugs.GithubWebhookReceiver do
   import Plug.Conn
   
@@ -17,37 +18,62 @@ defmodule Wa7vcWeb.Plugs.GithubWebhookReceiver do
   defp github_api(conn, _options) do
     case conn.method do
       "POST" -> 
-        key = Application.get_env(:wa7vc, Wa7vcWeb.Endpoint)[:github_webhook_secret]
+        key = Application.get_env(:wa7vc, 
+                                  Wa7vcWeb.Endpoint)[:github_webhook_secret]
         {:ok, body, _} = read_body(conn)
-        signature = case get_req_header(conn, "x-hub-signature") do
-          # Note that this will error out if the string following sha1= doesn't correctly base16 decode.
-          # Since that implies that this isn't a valid github webhook, we're okay with that.
-          ["sha1=" <> signature  | []] ->
-            case Base.decode16(signature, case: :lower) do
-              {:ok, sig} -> sig
-              _ -> :error
-            end
-          _ -> :error
-        end
-        decoded_sig = :crypto.hmac(:sha, key, body)
-        case decoded_sig do
-          ^signature ->
-            Phoenix.PubSub.broadcast(Wa7vc.PubSub, "webhook:received_raw", %{source: "github", body: body})
+
+        calculated_signature = :crypto.hmac(:sha, key, body)
+
+        case extract_github_signature(conn)
+             |> decode_base16_signature
+             |> signature_matches(calculated_signature) do
+          true ->
+            Phoenix.PubSub.broadcast(Wa7vc.PubSub,
+                                     "webhook:received_raw",
+                                     %{source: "github", body: body})
             conn
             |> put_resp_content_type("application/json")
-            |> send_resp(200, "{ \"code\":200, \"msg\":\"Ingested Webhook, thanks Github!\" }")
+            |> send_resp(200, "{ \"code\":200, "
+               <> "\"msg\":\"Ingested Webhook, thanks Github!\" }")
             |> halt
-          _ ->
+          false ->
             conn
             |> put_resp_content_type("application/json")
-            |> send_resp(401, "{ \"code\":401, \"msg\":\"Invalid Signature. You're not Github!\" }")
+            |> send_resp(401, "{ \"code\":401, "
+               <> "\"msg\":\"Invalid Signature. You're not Github!\" }")
             |> halt
         end
       _ -> 
         conn
         |> put_resp_content_type("application/json")
-        |> send_resp(405, "{ \"code\":405, \"msg\":\"Incorrect HTTP Method, who even are you?\" }")
+        |> send_resp(405, "{ \"code\":405, "
+           <> "\"msg\":\"Incorrect HTTP Method, who even are you?\" }")
         |> halt
     end
   end
+
+
+  #####
+  # Method chain for cleanly extracting and validating the signature attached
+  # to the webhook sent by github.
+  #####
+
+  defp extract_github_signature(conn) do
+    case get_req_header(conn, "x-hub-signature") do
+      ["sha1=" <> signature | []] -> signature
+      _ -> :error
+    end
+  end
+
+  defp decode_base16_signature(sig) do
+    case Base.decode16(sig, case: :lower) do
+      {:ok, decoded_sig} -> decoded_sig
+      _ -> :error
+    end
+  end
+
+  defp signature_matches(request_signature, calculated_signature) do
+    request_signature == calculated_signature
+  end
+
 end
