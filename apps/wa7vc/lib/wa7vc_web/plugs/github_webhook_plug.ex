2466a6ef9cf8111fc2461b8df496ff0bd66faec6
Josh Proehl <josh@daedalusdreams.com>
2020-10-14 12:43:40 -0500

    Github Webhook responses update, and handle receipt of non-sha1 value
    Rather than crash when we get a value in the x-hub-signature, we'll
    actually fail out with the invalid signutare response

--- apps/wa7vc/lib/wa7vc_web/plugs/github_webhook_plug.ex
+++ apps/wa7vc/lib/wa7vc_web/plugs/github_webhook_plug.ex
@@ -12,42 +12,41 @@ defmodule Wa7vcWeb.Plugs.GithubWebhookReceiver do
       ^mount -> github_api(conn, options)
       _ -> conn
     end
-    # if not mount matches => return
-    # if no signature => error
-    # if signature doesn't match => error
-    # done.
   end
 
-  def github_api(conn, _options) do
+  defp github_api(conn, _options) do
     case conn.method do
       "POST" -> 
         key = Application.get_env(:wa7vc, Wa7vcWeb.Endpoint)[:github_webhook_secret]
         {:ok, body, _} = read_body(conn)
         signature = case get_req_header(conn, "x-hub-signature") do
-          ["sha1=" <> signature  | []] -> 
-            {:ok, signature} = Base.decode16(signature, case: :lower) 
-            signature
-          x -> x
+          # Note that this will error out if the string following sha1= doesn't correctly base16 decode.
+          # Since that implies that this isn't a valid github webhook, we're okay with that.
+          ["sha1=" <> signature  | []] ->
+            case Base.decode16(signature, case: :lower) do
+              {:ok, sig} -> sig
+              _ -> :error
+            end
+          _ -> :error
         end
-        hmac = :crypto.hmac(:sha, key, body)
-        case hmac do
+        decoded_sig = :crypto.hmac(:sha, key, body)
+        case decoded_sig do
           ^signature ->
-            Phoenix.PubSub.broadcast(Wa7vc.PubSub, :raw_webhook_received, %{source: "github", body: body})
+            Phoenix.PubSub.broadcast(Wa7vc.PubSub, "webhook:received_raw", %{source: "github", body: body})
             conn
             |> put_resp_content_type("application/json")
-            |> send_resp(200, "{ \"code\":200, \"msg\":\"Handled Github Webhook\" }")
+            |> send_resp(200, "{ \"code\":200, \"msg\":\"Ingested Webhook, thanks Github!\" }")
             |> halt
           _ ->
-            #IO.inspect key, label: "Using Key: "
             conn
             |> put_resp_content_type("application/json")
-            |> send_resp(401, "{ \"code\":401, \"msg\":\"You're not github!\" }")
+            |> send_resp(401, "{ \"code\":401, \"msg\":\"Invalid Signature. You're not Github!\" }")
             |> halt
         end
       _ -> 
         conn
         |> put_resp_content_type("application/json")
-        |> send_resp(405, "{ \"code\":405, \"msg\":\"Incorrect HTTP method, who even are you?\" }")
+        |> send_resp(405, "{ \"code\":405, \"msg\":\"Incorrect HTTP Method, who even are you?\" }")
         |> halt
     end
   end
