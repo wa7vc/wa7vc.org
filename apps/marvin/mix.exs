a0b2e8c924832dc0bb2d2ee29e7a785610284817
Josh Proehl <josh@daedalusdreams.com>
2020-10-15 15:12:09 -0500

    Bump versions for another release

--- apps/marvin/mix.exs
+++ apps/marvin/mix.exs
@@ -4,7 +4,7 @@ defmodule Marvin.MixProject do
   def project do
     [
       app: :marvin,
-      version: "0.3.1",
+      version: "0.3.2",
       elixir: "~> 1.10",
       elixirc_path: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
