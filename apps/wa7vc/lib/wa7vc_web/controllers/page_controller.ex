c5d9477164224b4d0cf93ac2a7df8233c8b4dd8a
Josh Proehl <josh@daedalusdreams.com>
2021-05-21 23:26:02 -0500

    Add 2021 events. We're back baby!

--- apps/wa7vc/lib/wa7vc_web/controllers/page_controller.ex
+++ apps/wa7vc/lib/wa7vc_web/controllers/page_controller.ex
@@ -7,7 +7,7 @@ defmodule Wa7vcWeb.PageController do
   plug :dmr_meta_desc when action in [:dmrgathering]
 
   defp dmr_year(conn, _opts) do
-    valid_years = ["2019", "2020"]
+    valid_years = ["2019", "2020", "2021"]
     latest_year = List.last(valid_years)
     requested_year = conn.path_params["year"]
     
@@ -35,7 +35,7 @@ defmodule Wa7vcWeb.PageController do
   end
 
   defp sg_year(conn, _opts) do
-    valid_years = ["2018", "2019", "2020"]
+    valid_years = ["2018", "2019", "2020", "2021"]
     latest_year = List.last(valid_years)
     requested_year = conn.path_params["year"]
     
