f22c87469b0e8d220f49fc30abbec784a86d7968
Josh Proehl <josh@daedalusdreams.com>
2021-06-02 07:42:31 -0500

    Update Marvin :jason version to match wa7vc.
    (and fulfill Sentry deps)

--- apps/marvin/mix.exs
+++ apps/marvin/mix.exs
@@ -32,7 +32,7 @@ defmodule Marvin.MixProject do
       {:sentry, "~> 8.0"},
       {:phoenix_pubsub, "~> 2.0"},
       {:httpoison, "~> 1.6"},
-      {:jason, "~> 1.0"},     # Pin to same version as wa7vc_web uses
+      {:jason, "~> 1.1"},     # Pin to same version as wa7vc_web uses
       {:timex, "~> 3.1"},     # Pin to same version as wa7vc_web uses
       {:number, "~> 0.5.7"},  # Pin to same version as wa7vc_web uses
       {:aprs_parse, "~> 0.1.0"},
