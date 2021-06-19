a0b7d636341349f370ec4b1eb5201ea3bb4dbfbe
Josh Proehl <josh@daedalusdreams.com>
2021-06-19 18:21:07 -0500

    Run dev HTTP server on custom port.
    This allows us to develop multiple apps on the same machine without
    conflict.
    118 = ASCII code for v
    99 = ASCII code for c

--- apps/wa7vc/config/dev.exs
+++ apps/wa7vc/config/dev.exs
@@ -14,7 +14,7 @@ config :phoenix, :stacktrace_depth, 20
 # watchers to your application. For example, we use it
 # with brunch.io to recompile .js and .css sources.
 config :wa7vc, Wa7vcWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 11899],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
