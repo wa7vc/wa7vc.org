620934b2dbeba34e3da2358ece4d50ef83bd48e5
Josh Proehl <josh@daedalusdreams.com>
2021-08-07 22:55:27 -0500

    Fix ansible errors building apps
    The full ansible playbook should now work to build and deploy both apps,
    assuming the validation checks succeed!

--- apps/marvin/build_release.sh
+++ apps/marvin/build_release.sh
@@ -1,13 +1,10 @@
-  
 #!/bin/bash
 set -e
-
-mkdir -p tmp/
 
 docker build -f Dockerfile.releaser -t marvin:releaser .
 
 DOCKER_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
-docker run -ti --name marvin_releaser_${DOCKER_UUID} marvin:releaser /bin/true
+docker run --name marvin_releaser_${DOCKER_UUID} marvin:releaser /bin/true
 docker cp marvin_releaser_${DOCKER_UUID}:/opt/marvin.tar.gz tmp/
 docker rm marvin_releaser_${DOCKER_UUID}
--- apps/wa7vc/build_release.sh
+++ apps/wa7vc/build_release.sh
@@ -7,6 +7,6 @@ docker build -f Dockerfile.releaser -t wa7vc:releaser .
 
 DOCKER_UUID=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
-docker run -ti --name wa7vc_releaser_${DOCKER_UUID} wa7vc:releaser /bin/true
+docker run --name wa7vc_releaser_${DOCKER_UUID} wa7vc:releaser /bin/true
 docker cp wa7vc_releaser_${DOCKER_UUID}:/opt/wa7vc.tar.gz tmp/
 docker rm wa7vc_releaser_${DOCKER_UUID}
