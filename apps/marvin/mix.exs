ff466cf9e067fefa1117064cd505b5d50798032d
Josh Proehl <josh@daedalusdreams.com>
2021-08-20 11:12:45 -0500

    Version bump for dual-deploy

--- apps/marvin/mix.exs
+++ apps/marvin/mix.exs
@@ -4,7 +4,7 @@ defmodule Marvin.MixProject do
   def project do
     [
       app: :marvin,
-      version: "0.3.10",
+      version: "0.3.11",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
