8e62d9e5308876d3dc6c65efa689d3a327345c0e
Josh Proehl <josh@daedalusdreams.com>
2022-02-16 09:29:26 -0600

    Version bump for deploy

--- apps/wa7vc/mix.exs
+++ apps/wa7vc/mix.exs
@@ -4,7 +4,7 @@ defmodule Wa7vcWeb.Mixfile do
   def project do
     [
       app: :wa7vc,
-      version: "0.3.13",
+      version: "0.3.14",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
