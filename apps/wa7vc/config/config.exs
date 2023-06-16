2d5ed2d94dc5c29a891ee7c13fa4d26d76932cd0
Josh Proehl <josh@daedalusdreams.com>
2023-06-16 15:29:45 -0500

    Update tailwind version

--- apps/wa7vc/config/config.exs
+++ apps/wa7vc/config/config.exs
@@ -43,7 +43,7 @@ config :esbuild,
 
 # Configure tailwind (the version is required)
 config :tailwind,
-  version: "3.2.4",
+  version: "3.2.7",
   default: [
     args: ~w(
       --config=tailwind.config.js
