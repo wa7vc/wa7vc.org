b8ff387f1a18bc5e9b8991c6f4e80d32a3118c7a
Josh Proehl <josh@daedalusdreams.com>
2023-07-25 16:33:05 -0500

    Move to new app server and version bump for deploy

--- apps/marvin/mix.exs
+++ apps/marvin/mix.exs
@@ -4,7 +4,7 @@ defmodule Marvin.MixProject do
   def project do
     [
       app: :marvin,
-      version: "0.4.0",
+      version: "0.4.1",
       elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
